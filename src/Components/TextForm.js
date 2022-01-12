2f96494737ab10084d4c22d78fde78b1ec17b51d
Anujkumar070 <anujkumar070201@gmail.com>
2022-01-12 22:22:10 +0530

    Lowercase and Text Summary Added

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -13,14 +13,38 @@ export default function TextForm(props) {
         
     }
 
+    const handleLowerCase=()=>{
+        let newText=text.toLowerCase();
+        setText(newText);
+    }
 
+    const reset=()=>{
+        let newText="";
+        setText(newText);
+    }
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
             <textarea className="form-control" id="textbox" rows="10" value={text} onChange={handleChange} placeholder='Enter Your Text Here'></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpperCase}>Convert Into UpperCase</button>
+            <button className="btn btn-primary mx-1" onClick={handleUpperCase}>Convert Into UpperCase</button>
+            <button className="btn btn-primary mx-1" onClick={handleLowerCase}>Convert Into LowerCase</button>
+            <button className="btn btn-primary mx-1" onClick={reset}>Reset</button>
+
         </div>
+        <div className="container my-2">
+            <table>
+            <tr><th colspan="4"><u>Text Summary: </u></th></tr>
+            <tr><td><b>No. of Words:</b> {text.split(" ").length}  &#160; &#160;  &#160;  &#160; </td>
+            <td><b>No. of Characters: </b>{text.length}  &#160; &#160;  &#160;  &#160; </td>
+            <td><b>Time needed to read: </b>{text.length*0.008} minutes  </td></tr>
+            </table>
+            <h4><u>Preview: </u></h4>
+            <p>{text}</p>
+
+        </div>
+        </>
     )
 }
